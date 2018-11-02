9d82c7913b0f4ced9e4bd3e0c97fef7b08f26eb0
emmears <emears@nd.edu>
2018-11-01 22:13:42 -0400

    started problem 2, working out error messages

--- exercise8done.R
+++ exercise8done.R
@@ -8,11 +8,48 @@
 #summarized in "UWvMSU_1-22-13.txt" generate a graph
 #similar to the one I show above.
 
-UWvsMSU = read.table("UWvMSU_1-22-13.txt",header=T,)
+#read in data
+UWvsMSU = read.table("UWvMSU_1-22-13.txt",header=T,sep = "\t")
 head(UWvsMSU)
+#add a cumulative score column
+UWvsMSU$cummulativescore = 0
+#separate out the scores
 UW= UWvsMSU[which(UWvsMSU$team == "UW"),]
-for (i in UW){
-  cumm = UW$score[i]+UW$score[i-1]
-  }
-return(cumm)
-plot(UW$time,UW$score, type = "l")
+MSU = UWvsMSU[which(UWvsMSU$team == "MSU"),]
+#fill cumullative score columns 
+UW$cummulativescore = cumsum(UW$score)
+MSU$cummulativescore = cumsum(MSU$score)
+#plot UW
+Time = UW$time
+Cumm = UW$cummulativescore
+plot(Time, Cumm,type = "l")
+#plot MSU 
+time =MSU$time
+cumm =MSU$cummulativescore
+lines(time,cumm, col = "blue")
+
+
+#####PROBLEM 2#######
+#Write a game called "guess my number". 
+#The computer will generate a random number between 1 and 100. 
+#The user types in a number and the computer replies "lower"
+#if the random number is lower than the guess, 
+#"higher" if the random number is higher, 
+#and "correct!" if the guess is correct. 
+#The player can continue guessing until they get it right. 
+#A couple tips on this one: 
+#. take a look at the Input/Output reference I gave you for 
+   #how to get input from the user 
+#. sample() is a function that allows for a random selection
+   #from a vector containing a set of integers 
+num = sample(1:100)
+guess = 50
+if(guess == num){
+  print("Correct!")
+}else if (guess > num){
+  print("Lower")
+}else{
+  print("Higher")
+}
+
+
